ae6ade3801e3a572a713b88b3d19d41d88bdd651
zookee <z.persboy@gmail.com>
2024-06-24 22:09:01 +0100

    Ignore unexported in cmp.Diff for specific types

--- mock_server.go
+++ mock_server.go
@@ -90,7 +90,7 @@ func (s *mockServer) popRPC(gotReq proto.Message) (interface{}, error) {
 		// 	return nil, fmt.Errorf("mockServer: failed to marshal want request: %v", err)
 		// }
 		if !proto.Equal(gotReq, ri.wantReq) {
-			diff := cmp.Diff(gotReq, ri.wantReq, cmp.Comparer(proto.Equal), cmpopts.EquateEmpty())
+			diff := cmp.Diff(gotReq, ri.wantReq, cmpopts.IgnoreUnexported(pb.CommitRequest{}, pb.Mutation{}))
 			return nil, fmt.Errorf("mockServer: bad request\ngot:%T\nwant:%T\n-got\n+want:\n%s",
 				gotReq,
 				ri.wantReq,
